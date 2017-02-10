8805635dd74f9dbf5dbbcdb9e60e2448359324b4
Gedas <gedbaciulis@gmail.com>
2017-02-10 14:22:23 +0200

    feat: add pngquant

--- generators/app/templates/gulpfile.js/tasks/images.js
+++ generators/app/templates/gulpfile.js/tasks/images.js
@@ -5,18 +5,14 @@ var changed = require('gulp-changed');
 var config = require('../config');
 var gulp = require('gulp');
 var handleErrors = require('../lib/handleErrors');
-var imagemin = require('gulp-imagemin');
+// var imagemin = require('gulp-imagemin');
 var plumber = require('gulp-plumber');
-var pngquant = require('imagemin-pngquant');
+var gulpPngquant = require('gulp-pngquant');
 
 gulp.task('images', function() {
   return gulp.src(config.tasks.images.src)
     .pipe(plumber({ errorHandler: handleErrors }))
-    .pipe(imagemin({
-      use: [pngquant({
-            quality: '65-80'
-        })]
-    }))
+    .pipe(gulpPngquant( {quality: '65-80'} ))
     .pipe(changed(config.tasks.images.dest))
     .pipe(gulp.dest(config.tasks.images.dest))
     .pipe(browserSync.stream());
