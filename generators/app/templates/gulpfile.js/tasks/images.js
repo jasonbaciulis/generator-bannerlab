6903f4ddad23e25d9723e93f6904fe58fa2a0570
Gediminas Baciulis <gedbaciulis@gmail.com>
2017-02-11 12:23:29 +0200

    fix: imagemin-pngquant

--- generators/app/templates/gulpfile.js/tasks/images.js
+++ generators/app/templates/gulpfile.js/tasks/images.js
@@ -7,12 +7,17 @@ var gulp = require('gulp');
 var handleErrors = require('../lib/handleErrors');
 var imagemin = require('gulp-imagemin');
 var plumber = require('gulp-plumber');
-var gulpPngquant = require('gulp-pngquant');
+// var gulpPngquant = require('gulp-pngquant');
+var imageminPngquant = require('imagemin-pngquant');
 
 gulp.task('images', function() {
   return gulp.src(config.tasks.images.src)
     .pipe(plumber({ errorHandler: handleErrors }))
-    .pipe(gulpPngquant( {quality: '50-60'} ))
+    // .pipe(gulpPngquant( {quality: '55-65'} ))
+    .pipe(imagemin({
+        use: [imageminPngquant() ] }).then( () => {
+          console.log('Images optimized');
+    }))
     .pipe(changed(config.tasks.images.dest))
     .pipe(gulp.dest(config.tasks.images.dest))
     .pipe(browserSync.stream());
